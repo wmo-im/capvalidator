f7c2227c6b649dc7de769676d9dd44cc70af8a0b
RoryPTB <47696929+RoryPTB@users.noreply.github.com>
2024-07-03 17:30:12 +0200

    init: Initial project setup

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# cap-validator
+# The CAP Validator
+
+### The Python module for ensuring the validity of CAP alerts.
+
+<a href="https://github.com/wmo-im/wis2-downloader/blob/main/LICENSE" alt="License" ><img src="https://img.shields.io/badge/License-Apache_2.0-blue"></img></a>
+
+## Features
+
+- Validating the alert against the OASIS CAP 1.2 schema.
+- File integrity checking.
+- Digital signature verification.
+
+
+## Getting Started
+
+### 1. Installation
+
+__NOTE__: The downloader has not yet been uploaded to PyPI and needs to be installed directly from GitHub:
+
+```bash
+pip install https://github.com/wmo-im/cap-validator/archive/main.zip
+```
---
+++ cap_validator/__init__.py
@@ -0,0 +1,60 @@
+###############################################################################
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+###############################################################################
+
+from cap_validator.validate_schema import CheckSchema
+from cap_validator.check_integrity import CheckIntegrity
+from cap_validator.verify_signature import VerifySignature
+
+__version__ = '0.1.dev0'
+
+
+class ValidationResult:
+    def __init__(self, status, message):
+        self.status = status
+        self.message = message
+
+
+def validate_cap(cap_bytes) -> ValidationResult:
+    """Performs the three steps of CAP validation: schema validation,
+    integrity check, and signature verification.
+
+    Args:
+        cap_bytes (bytes): The CAP alert XML file to be validated.
+
+    Returns:
+        ValidationResult: The validation status and the associated message
+        justifying the status.
+    """
+    # Draft code to demonstrate the process of CAP validation
+
+    follows_schema = CheckSchema(cap_bytes)
+    if not follows_schema:
+        return ValidationResult(False, "CAP alert does not follow the schema.")
+
+    hash_matches = CheckIntegrity(cap_bytes)
+    if not hash_matches:
+        return ValidationResult(False, "CAP alert hash does not match the content.") # noqa
+
+    signature_valid = VerifySignature(cap_bytes)
+    if not signature_valid:
+        return ValidationResult(False, "CAP alert has not been signed or the signature is not valid.") # noqa
+
+    return ValidationResult(True, "CAP alert is valid.")
