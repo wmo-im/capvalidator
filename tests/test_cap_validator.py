b139b3ab66a857870c52da4ee658dfab7b4482c5
RoryPTB <47696929+RoryPTB@users.noreply.github.com>
2024-07-04 11:55:11 +0200

    test: add initial unit tests

---
+++ tests/test_cap_validator.py
@@ -0,0 +1,73 @@
+###############################################################################
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+###############################################################################
+
+import pytest
+from cap_validator.validate_schema import CheckSchema
+from cap_validator.check_integrity import CheckIntegrity
+from cap_validator.verify_signature import VerifySignature
+
+
+@pytest.fixture
+def valid_alert():
+    with open('data/seychelles.xml', 'rb') as f:
+        return f.read()
+
+
+@pytest.fixture
+def no_identifier():
+    with open('data/no_identifier.xml', 'rb') as f:
+        return f.read()
+
+
+@pytest.fixture
+def incorrect_digest():
+    with open('data/incorrect_digest.xml', 'rb') as f:
+        return f.read()
+
+
+@pytest.fixture
+def incorrect_signature():
+    with open('data/incorrect_signature.xml', 'rb') as f:
+        return f.read()
+
+
+def test_valid_alert(valid_alert):
+    assert CheckSchema(valid_alert)
+    assert CheckIntegrity(valid_alert)
+    assert VerifySignature(valid_alert)
+
+
+def test_no_identifier(no_identifier):
+    assert not CheckSchema(no_identifier)
+    assert not CheckIntegrity(no_identifier)
+    assert VerifySignature(no_identifier)
+
+
+def test_incorrect_digest(incorrect_digest):
+    assert CheckSchema(incorrect_digest)
+    assert not CheckIntegrity(incorrect_digest)
+    assert VerifySignature(incorrect_digest)
+
+
+def test_incorrect_signature(incorrect_signature):
+    assert CheckSchema(incorrect_signature)
+    assert CheckIntegrity(incorrect_signature)
+    assert not VerifySignature(incorrect_signature)
