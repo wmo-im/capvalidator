4ffe8be05a8755f528e7c8f5029d4f338653827a
RoryPTB <47696929+RoryPTB@users.noreply.github.com>
2024-07-08 16:59:47 +0200

    tests: correct fixture paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,23 +3,27 @@ import pytest
 
 @pytest.fixture
 def valid_alert():
-    with open('data/seychelles.xml', 'r', encoding='utf-8') as f:
+    with open('tests/data/seychelles.xml', 'r',
+              encoding='utf-8') as f:
         return f.read()
 
 
 @pytest.fixture
 def no_identifier():
-    with open('data/no_identifier.xml', 'r', encoding='utf-8') as f:
+    with open('tests/data/no_identifier.xml', 'r',
+              encoding='utf-8') as f:
         return f.read()
 
 
 @pytest.fixture
 def incorrect_digest():
-    with open('data/incorrect_digest.xml', 'r', encoding='utf-8') as f:
+    with open('tests/data/incorrect_digest.xml', 'r',
+              encoding='utf-8') as f:
         return f.read()
 
 
 @pytest.fixture
 def incorrect_signature():
-    with open('data/incorrect_signature.xml', 'r', encoding='utf-8') as f:
+    with open('tests/data/incorrect_signature.xml', 'r',
+              encoding='utf-8') as f:
         return f.read()
